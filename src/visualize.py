5edb5df4b7a5dd0fd7bb9a47b09b7543be512e88
Michael Perna <michael.anthony.perna@gmail.com>
2019-04-21 01:56:23 -0700

    Visualization now properly displays an image in grayscale

--- src/visualize.py
+++ src/visualize.py
@@ -19,21 +19,24 @@ def visualize_input(img, ax):
     for x in range(width):
         for y in range(height):
             ax.annotate(str(round(img[x][y], 2)), xy=(y, x),
-                        horizontalalignment='center',
-                        verticalalignment='center',
-                        color='white' if img[x][y] < thresh else 'black')
+                  horizontalalignment='center',
+                  verticalalignment='center',
+                  color='white' if img[x][y] < thresh else 'black')
 
 
 # Load a color image in grayscale
-image = imread('../data/rps/paper/20190403_143158.jpg', as_grey=True)
+image = imread('../data/resized/128/scissors/2019-04-03_13-17-35_1.jpg', as_gray=True)
 image = resize(image, (64, 64), mode='reflect')
+
 print('This image is: ', type(image),
       'with dimensions:', image.shape)
 
 plt.imshow(image, cmap='gray')
 
-fig = plt.figure(figsize=(8, 8))
+
+fig = plt.figure(figsize=(16, 16))
 ax = fig.add_subplot(111)
 visualize_input(image, ax)
+
 
 plt.show()
