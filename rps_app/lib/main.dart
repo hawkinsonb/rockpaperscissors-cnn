d540c6299734afd7b653199c6450c61243356e95
Brandon Hawkinson <hawkinson.w.b@gmail.com>
2019-04-29 19:26:14 -0700

    now this commit really finished up the app

--- rps_app/lib/main.dart
+++ rps_app/lib/main.dart
@@ -4,10 +4,12 @@ import 'package:tflite/tflite.dart';
 import 'dart:io';
 import 'package:image/image.dart' as ImageFluxer;
 import 'package:path_provider/path_provider.dart';
+import 'dart:math';
 
 List<CameraDescription> cameras;
 
-void logError (String code, String message) => print('Error: $code\nError Message: $message');
+void logError(String code, String message) =>
+    print('Error: $code\nError Message: $message');
 
 Future<void> main() async {
   try {
@@ -23,14 +25,13 @@ Future<void> main() async {
 Future<Null> _loadModel() async {
   try {
     String res = await Tflite.loadModel(
-    model: "assets/rps.tflite",
-    labels: "assets/labels.txt",
-    numThreads: 1); // defaults to 1
+        model: "assets/rps.tflite",
+        labels: "assets/labels.txt",
+        numThreads: 1); // defaults to 1
     print(res);
-  } catch(e) {
+  } catch (e) {
     print('Error: $e.code\nError Message: $e.message');
   }
-
 }
 
 class MyApp extends StatelessWidget {
@@ -58,10 +59,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   CameraController _controller;
+  String _prediction = "";
+  String _random = "";
+  String _state = "";
 
   @override
   void initState() {
-    _controller =CameraController(cameras[0], ResolutionPreset.high);
+    _controller = CameraController(cameras[0], ResolutionPreset.high);
     _controller.initialize().then((_) {
       if (!mounted) {
         return;
@@ -76,23 +80,58 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  void appLogic(String filePath) {
-      var recognitions = Tflite.runModelOnImage(
-          path: filePath
-      );
+  Future appLogic(String filePath) async {
+    String res = await Tflite.loadModel(
+      model: "assets/rps.tflite",
+      labels: "assets/labels.txt",
+    );
 
-      print(recognitions);
+    var recognitions = await Tflite.runModelOnImage(
+      path: filePath,
+    );
+
+    _prediction = recognitions.elementAt(0)["label"];
+
+    var rng = new Random();
+    var guess = rng.nextInt(100);
+
+    if (guess <= 32) {
+      _random = "rock";
+    } else if (guess > 32 && guess <= 65) {
+      _random = "paper";
+    } else if (guess > 65 && guess <= 99) {
+      _random = "scissors";
+    }
+
+    if (_random == "scissors" && _prediction == "paper") {
+      _state = "You lose, scissors cuts paper!";
+    } else if (_random == "paper" && _prediction == "rock") {
+      _state = "You lose, paper covers rock!";
+    } else if (_random == "rock" && _prediction == "scissors") {
+      _state = "You lose, rock smashes scissors!";
+    } else if (_prediction == "scissors" && _random == "paper") {
+      _state = "You win, scissors cuts paper!";
+    } else if (_prediction == "paper" && _random == "rock") {
+      _state = "You win, paper covers rock!";
+    } else if (_prediction == "rock" && _random == "scissors") {
+      _state = "You win, rock smashes scissors!";
+    } else {
+      _state = "Draw or unknown";
+    }
+
+    setState(() {});
   }
 
   void _takePicturePressed() {
     takePicture().then((String filePath) {
       if (mounted) {
         print("US" + filePath);
-        ImageFluxer.Image img = ImageFluxer.decodeImage(new File(filePath).readAsBytesSync());
+        ImageFluxer.Image img =
+            ImageFluxer.decodeImage(new File(filePath).readAsBytesSync());
 
         ImageFluxer.Image smallImage = ImageFluxer.copyResize(img, 128);
 
-        new File(filePath).writeAsBytesSync(ImageFluxer.encodePng(smallImage));
+        new File(filePath).writeAsBytesSync(ImageFluxer.encodeJpg(smallImage));
       }
       appLogic(filePath);
     });
@@ -105,7 +144,7 @@ class _MyHomePageState extends State<MyHomePage> {
       print("Controller is not initialized");
       return null;
     }
-    
+
     final Directory extDir = await getApplicationDocumentsDirectory();
     final String dirPath = '${extDir.path}/Pictures/flutter_test';
     await Directory(dirPath).create(recursive: true);
@@ -153,7 +192,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   onPressed: _takePicturePressed,
                 ),
               ),
-            )
+            ),
+            Expanded(flex: 1, child: Text("You picked: $_prediction")),
+            Expanded(flex: 1, child: Text("Computer picked: $_random")),
+            Expanded(flex: 1, child: Text("$_state")),
           ],
         ),
       ),
