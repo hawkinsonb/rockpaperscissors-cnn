998af988ff65456569b0ec3ae656444aec16f0bc
Brandon Hawkinson <hawkinson.w.b@gmail.com>
2019-05-01 17:18:50 -0700

    new trained model and more data

--- src/cnn-flow.py
+++ src/cnn-flow.py
@@ -59,9 +59,14 @@ model = Sequential()
 model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu',
                  input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=2))
-model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(Conv2D(filters=32, kernel_size=2,
+                 padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=2))
-model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=2,
+                 padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=128, kernel_size=2,
+                 padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.4))
 model.add(Flatten())
