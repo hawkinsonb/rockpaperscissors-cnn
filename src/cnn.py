f5fc50f987dfdaf7b8b3264e66dc525d9184aa09
Hawkinsonb <hawkinson.w.b@gmail.com>
2019-05-03 02:34:58 -0700

    this should work

--- src/cnn.py
+++ src/cnn.py
@@ -42,28 +42,25 @@ callbacks_list = [checkpoint]
 # define the model
 model = Sequential()
 
-model.add(Conv2D(filters=2, kernel_size=(3, 3),
-                 padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
+
 model.add(Conv2D(filters=4, kernel_size=(3, 3),
-                 padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
+                 padding='same', activation='relu', input_shape=input_shape))
 model.add(Conv2D(filters=8, kernel_size=(3, 3),
                  padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.3))
+model.add(Conv2D(filters=12, kernel_size=(3, 3),
+                 padding='same', activation='relu'))
 model.add(Conv2D(filters=16, kernel_size=(3, 3),
                  padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.3))
 model.add(Conv2D(filters=20, kernel_size=(3, 3),
                  padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
 model.add(Conv2D(filters=32, kernel_size=(3, 3),
                  padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.3))
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(128, activation='relu'))
 model.add(Dense(3, activation='softmax'))
 
 opt = keras.optimizers.RMSprop(lr=1e-04, decay=1e-6)
